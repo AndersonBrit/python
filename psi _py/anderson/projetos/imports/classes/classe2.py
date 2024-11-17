bf5ec898d115d6f52ebe36d25b03e9b86c37977b
Anderson <and3rsonbrit00@gmail.com>
2024-11-17 15:55:24 +0000

    Add files via upload

---
+++ psi _py/anderson/projetos/imports/classes/classe2.py
@@ -0,0 +1,6 @@
+from classe2 import Pessoa
+
+p1 = Pessoa("Anderson", 15)
+
+print(p1.name)
+print(p1.age)
---
+++ psi _py/anderson/projetos/imports/funcoes/funcao3.py
@@ -0,0 +1,4 @@
+from ficheiro2 import multiplicacao
+
+num1 = multiplicacao(2, 3, 10)
+print(num1)
