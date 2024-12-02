0202d7604c57f634d6bd3a2a03d7130faf6ec1bd
Anderson <and3rsonbrit00@gmail.com>
2024-12-02 06:49:22 +0000

    Delete psi_py/anderson directory

--- psi_py/anderson/aula/aulas/anderson_10_10_2024.py
+++
@@ -1,7 +0,0 @@
-lista = ["a","b","c"]
-i = 3
-
-for i in lista:
-    lista.append("dee")
-    print(lista)
-    break
--- psi_py/anderson/oop/classes_intro_obj.py
+++
@@ -1,8 +0,0 @@
-from classes_intro_blueprint import Pessoa
-from classes_intro_blueprint import Carro
-
-pessoa = Pessoa("João", 16)
-carro = Carro("Tesla", "Model 3")
-
-print(pessoa.saudacao())
-print(carro.informacao_do_carro())
